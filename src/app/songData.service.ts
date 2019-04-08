85651d99b11d8b9391637af1d44550a75fb29de1
Benedictiknkeonye@gmail.com <Benedictiknkeonye@gmail.com>
2019-04-08 11:29:27 +0100

    Improved authentication & enabled logout

--- src/app/songData.service.ts
+++ src/app/songData.service.ts
@@ -1,15 +1,26 @@
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import { Song } from './votes.model';
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+import { Song } from "./votes.model";
+import { AuthService } from "./auth/auth.service";
 
 @Injectable()
 export class SongDataService {
-    constructor(private http: Http) {}
-    storeSongs(songs: Song[]) {
-        return this.http.post('https://musicvoter-3bafe.firebaseio.com/data.json', songs);
-    }
+  constructor(private http: Http, private authService: AuthService) {}
 
-    getSongs(songs: Song[]) {
-        return this.http.get('https://musicvoter-3bafe.firebaseio.com/data.json');
-    }
+  // This method sends the HTTP request to Firebase to save the songs entered by the user
+  storeSongs(songs: Song[]) {
+    const token = this.authService.getToken();
+    return this.http.post(
+      "https://musicvoter-3bafe.firebaseio.com/data.json?auth=" + token,
+      songs
+    );
+  }
+
+  // This method sends the HTTP request to Firebase to retrieve the songs previously saved by the user
+  getSongs(songs: Song[]) {
+    const token = this.authService.getToken();
+    return this.http.get(
+      "https://musicvoter-3bafe.firebaseio.com/data.json?auth=" + token
+    );
+  }
 }
